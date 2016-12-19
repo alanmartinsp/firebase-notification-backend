4f0344d2dfedd58c35dcc4c43afbe6adfbf3ec92
Alan Martins <alan.m21@hotmail.com>
2016-12-19 12:46:36 -0200

    Criando Request

---
+++ app/Common/Requests/Request.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Request
+ *
+ * @author Alan
+ */
+class Request
+{
+    //put your code here
+}
--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -6,5 +6,22 @@ use Illuminate\Foundation\Http\FormRequest;
 
 abstract class Request extends FormRequest
 {
-    //
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'usu_token' => 'required'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        $message = $validator->getMessageBag();
+        abort(401, json_encode($message));
+    }
 }
